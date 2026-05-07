9f05340d9b113dd5ed41a2fdde85d6daa631514e
cary-rowen <manchen_0528@outlook.com>
2026-05-07 22:50:13 +0800

    Bump version, compatible with NVDA 2026.1.0

--- buildVars.py
+++ buildVars.py
@@ -23,10 +23,12 @@ addon_info = AddonInfo(
 	# Translators: Long description to be shown for this add-on on add-on information from add-on store
 	addon_description=_("""Aggregating various offline and online OCR services."""),
 	# version
-	addon_version="3.2.0",
+	addon_version="3.2.1",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
-	addon_changelog=_("""Compatible with NVDA 2 0 2 5.1 and above."""),
+	addon_changelog=_("""### 3.2.1
+
+Compatible with NVDA 2026.1."""),
 	# Author(s)
 	addon_author="huaiyinfeilong <huaiyinfeilong@163.com>",
 	# URL for the add-on documentation support
@@ -38,7 +40,7 @@ addon_info = AddonInfo(
 	# Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
 	addon_minimumNVDAVersion="2024.1.0",
 	# Last NVDA version supported/tested (e.g. "2024.4.0", ideally more recent than minimum version)
-	addon_lastTestedNVDAVersion="2025.1.0",
+	addon_lastTestedNVDAVersion="2026.1.0",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
